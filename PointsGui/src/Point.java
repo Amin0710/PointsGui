5984d6053d92da4f98fce8e60819e8afb72730cc
nurul <nurul@AminAlien.modem>
2021-10-15 12:01:15 +1100

    points locations

---
+++ PointsGui/src/Point.java
@@ -0,0 +1,26 @@
+
+
+public class Point {
+
+	// variable declaration
+	float x;
+	float y;
+
+	//creating the getter and setter for declared variables
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+}
